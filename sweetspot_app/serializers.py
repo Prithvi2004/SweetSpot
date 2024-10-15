93f7f84ea8bd5e198215206d1536547f09af6bca
M Prithvi <madivadapr@gmail.com>
2024-10-15 22:00:17 +0530

    SweetSpot working with DB

---
+++ sweetspot_app/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Customer, Cake, CakeCustomization, Cart, Order
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+class CakeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cake
+        fields = '__all__'
+
+class CakeCustomizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CakeCustomization
+        fields = '__all__'
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
